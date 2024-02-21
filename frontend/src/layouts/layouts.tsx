7669ed8d1331ec23474804f9acb800dcccb220e2
kike-pinto <cripintort@gmail.com>
2024-02-21 15:07:34 -0300

    User profile form

--- frontend/src/layouts/layouts.tsx
+++ frontend/src/layouts/layouts.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import Footer from '@/components/Footer'
 
 type Props = {
   children: React.ReactNode
+  showHero?: boolean
 }
 
-const Layout = ({ children }: Props) => {
+const Layout = ({ children, showHero = false }: Props) => {
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
-      <Hero />
+      {showHero && <Hero />}
       <div className='container mx-auto flex-1 py-10'>{children}</div>
       <Footer />
     </div>
