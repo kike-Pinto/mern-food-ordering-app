573a607ac8afe7c71bdac63b392d727c9f6e4f28
kike-pinto <cripintort@gmail.com>
2024-02-27 17:12:19 -0300

    pagination

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,4 +1,5 @@
 import { useSearchRestaurants } from '@/api/RestaurantApi'
+import PaginationSelector from '@/components/PaginationSelector'
 import SearchBar, { SearchForm } from '@/components/SearchBar'
 import SearchResultCard from '@/components/SearchResultCard'
 import SearchResultInfo from '@/components/SearchResultInfo'
@@ -7,14 +8,23 @@ import { useParams } from 'react-router-dom'
 
 export type SearchState = {
   searchQuery: string
+  page: number
 }
 
 const SearchPage = () => {
   const { city } = useParams()
   const [searchState, setSearchState] = useState<SearchState>({
     searchQuery: '',
+    page: 1,
   })
   const { results, isLoading } = useSearchRestaurants(searchState, city)
+
+  const setPage = (page: number) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      page,
+    }))
+  }
 
   const setSearchQuery = (searchFormData: SearchForm) => {
     setSearchState((prevState) => ({
@@ -53,6 +63,11 @@ const SearchPage = () => {
         {results.data.map((restaurant) => (
           <SearchResultCard restaurant={restaurant} />
         ))}
+        <PaginationSelector
+          page={results.pagination.page}
+          pages={results.pagination.pages}
+          onPageChange={setPage}
+        />
       </div>
     </div>
   )
