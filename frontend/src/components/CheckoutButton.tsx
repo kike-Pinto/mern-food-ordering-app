c8363ac9173d5954355ad5f2960302450c0d3ae5
kike-pinto <cripintort@gmail.com>
2024-03-01 19:04:33 -0300

    update user profile form

--- frontend/src/components/CheckoutButton.tsx
+++ frontend/src/components/CheckoutButton.tsx
@@ -57,8 +57,8 @@ const CheckoutButton = ({ onCheckout, disabled }: Props) => {
           currentUser={currentUser}
           onSave={onCheckout}
           isLoading={isGetUserLoading}
-          // title='Confirm Delivery Details'
-          // buttonText='Continue to payment'
+          title='Confirm Delivery Details'
+          buttonText='Continue to payment'
         />
       </DialogContent>
     </Dialog>
