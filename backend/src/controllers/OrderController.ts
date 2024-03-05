ae79cca400bba36481f4e9bf09b49b228d5fe614
kike-pinto <cripintort@gmail.com>
2024-03-05 16:11:05 -0300

    Update order in stripe webhook

--- backend/src/controllers/OrderController.ts
+++ backend/src/controllers/OrderController.ts
@@ -5,13 +5,14 @@ import Order from '../models/order'
 
 const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string)
 const FRONTEND_URL = process.env.FRONTEND_URL as string
+const STRIPE_ENDPOINT_SECRET = process.env.STRIPE_WEBHOOK_SECRET as string
 
-const stripeWebhookHandler = async (req: Request, res: Response) => {
-  console.log('RECIEVED EVENT')
-  console.log('==============')
-  console.log('event: ', req.body)
-  res.send()
-}
+// const stripeWebhookHandler = async (req: Request, res: Response) => {
+//   console.log('RECIEVED EVENT')
+//   console.log('==============')
+//   console.log('event: ', req.body)
+//   res.send()
+// }
 
 type CheckoutSessionRequest = {
   cartItems: {
@@ -26,6 +27,37 @@ type CheckoutSessionRequest = {
     city: string
   }
   restaurantId: string
+}
+
+const stripeWebhookHandler = async (req: Request, res: Response) => {
+  let event
+
+  try {
+    const sig = req.headers['stripe-signature']
+    event = STRIPE.webhooks.constructEvent(
+      req.body,
+      sig as string,
+      STRIPE_ENDPOINT_SECRET
+    )
+  } catch (error: any) {
+    console.log(error)
+    return res.status(400).send(`Webhook error: ${error.message}`)
+  }
+
+  if (event.type === 'checkout.session.completed') {
+    const order = await Order.findById(event.data.object.metadata?.orderId)
+
+    if (!order) {
+      return res.status(404).json({ message: 'Order not found!' })
+    }
+
+    order.totalAmount = event.data.object.amount_total
+    order.status = 'paid'
+
+    await order.save()
+  }
+
+  res.status(200).send()
 }
 
 const createCheckoutSession = async (req: Request, res: Response) => {
